785d14ad691ab586f2d0d39bee2b2b08ce525c68
Water <water02120212@gmail.com>
2025-04-19 14:24:47 +0800

    feat: second version-print the second member name

--- FirstClass17Team.java
+++ FirstClass17Team.java
@@ -2,5 +2,6 @@ public class FirstClass17Team {
     public static void main(String[] args) {
         System.out.println("這是用來練習GitHub協作的程式!");
         System.out.println("\"學號：\"01257026\"\n姓名：\"蔡孟睿\"");
+        System.out.println("\"學號：\"01257020\"\n姓名：\"張肇豐\"");
     }
 }
