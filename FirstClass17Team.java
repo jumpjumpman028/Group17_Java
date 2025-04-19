4ec370e10172a819fc1a6ec587778a6e41a0f32f
jumpjumpman <145435018+jumpjumpman028@users.noreply.github.com>
2025-04-19 14:21:08 +0800

    組長commit
    01257026蔡孟睿

---
+++ FirstClass17Team.java
@@ -0,0 +1,6 @@
+public class FirstClass17Team {
+    public static void main(String[] args) {
+        System.out.println("這是用來練習GitHub協作的程式!");
+        System.out.println("\"學號：\"01257026\"\n姓名：\"蔡孟睿\"");
+    }
+}
